85983e4b9da0eae595dd0c84ff1966a4dd48532f
wentjiang <wentjiang@sina.com>
2020-08-27 22:53:04 +0800

    refactor the test code

--- src/test/java/com/wentjiang/locker/LockerTest.java
+++ src/test/java/com/wentjiang/locker/LockerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class LockerTest {
 
+    private static int DEFAULT_CAPACITY = 10;;
+
     @Test
     public void test() {
         System.out.println("hello test");
@@ -12,7 +14,7 @@ public class LockerTest {
 
     @Test
     public void should_generate_ticket_when_locker_not_full() {
-        Locker locker = new Locker(10);
+        Locker locker = new Locker(DEFAULT_CAPACITY);
         Bag bag = new Bag("test name");
         Ticket ticket = locker.storeBag(bag);
         Assert.assertNotNull(ticket);
