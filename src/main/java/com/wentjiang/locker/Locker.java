493b995f3813101b55154a60c083c92912367a19
wentjiang <wentjiang@sina.com>
2020-08-27 19:57:06 +0800

    init project

--- src/main/java/com/wentjiang/locker/Locker.java
+++ src/main/java/com/wentjiang/locker/Locker.java
@@ -1,4 +1,9 @@
 package com.wentjiang.locker;
 
 public class Locker {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
 }
--- src/test/java/com/wentjiang/locker/LockerTest.java
+++ src/test/java/com/wentjiang/locker/LockerTest.java
@@ -1,4 +1,12 @@
 package com.wentjiang.locker;
 
+import org.junit.jupiter.api.Test;
+
 public class LockerTest {
+
+    @Test
+    public void test(){
+        System.out.println("hello test");
+    }
+
 }
