ee76b5e43dd3a5f187f4f34c8076efee2f60e5b8
wentjiang <wentjiang@sina.com>
2020-09-01 21:39:25 +0800

    2nd test,primary locker robot store bag into fullLocker and notFullLocker success

--- src/test/java/com/wentjiang/locker/PrimaryLockerRobotTest.java
+++ src/test/java/com/wentjiang/locker/PrimaryLockerRobotTest.java
@@ -11,10 +11,22 @@ public class PrimaryLockerRobotTest {
     private static final int DEFAULT_CAPACITY = 10;
 
     @Test
-    public void should_store_bag_and_return_ticket_when_PrimaryLockerRobot_store_bag_given_PrimaryLockerRobot_many_not_full_locker(){
+    public void should_store_bag_and_return_ticket_when_PrimaryLockerRobot_store_bag_given_PrimaryLockerRobot_manager_not_full_locker() {
         List<Locker> lockers = new ArrayList<>();
         lockers.add(new Locker(DEFAULT_CAPACITY));
         lockers.add(new Locker(DEFAULT_CAPACITY));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(lockers);
+        Ticket ticket = primaryLockerRobot.storeBag(new Bag());
+        Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_store_bag_and_return_ticket_when_PrimaryLockerRobot_store_bag_given_PrimaryLockerRobot_manager_first_full_locker_and_second_not_full() {
+        List<Locker> lockers = new ArrayList<>();
+        Locker fullLocker = new Locker(DEFAULT_CAPACITY);
+        Locker notFullLocker = new Locker(DEFAULT_CAPACITY);
+        lockers.add(fullLocker);
+        lockers.add(notFullLocker);
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(lockers);
         Ticket ticket = primaryLockerRobot.storeBag(new Bag());
         Assertions.assertNotNull(ticket);
