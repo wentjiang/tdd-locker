f3391fa4756301364e39512e3f7765fc31e11b60
wentjiang <wentjiang@sina.com>
2020-09-12 00:18:20 +0800

    test 3 4 director manage two robots director have manager not manager locker and robot

--- src/test/java/com/wentjiang/locker/LockerRobotDirectorTest.java
+++ src/test/java/com/wentjiang/locker/LockerRobotDirectorTest.java
@@ -11,9 +11,11 @@ public class LockerRobotDirectorTest {
 
     @Test
     public void should_output_right_result_when_statistical_form_given_director_manage_1_manager_that_manage_two_lockers() {
-        String exceptedResult = "M 6 20\n" +
-                "  L 2 10\n" +
-                "  L 4 10\n";
+        String exceptedResult =
+                "" +
+                        "M 6 20\n" +
+                        "  L 2 10\n" +
+                        "  L 4 10\n";
         List<Locker> lockers = Arrays.asList(LockerUtil.getStoredLocker(10 - 2, 10),
                 LockerUtil.getStoredLocker(10 - 4, 10));
         LockerRobotManager manager = new LockerRobotManager(lockers, Collections.emptyList());
@@ -24,10 +26,12 @@ public class LockerRobotDirectorTest {
 
     @Test
     public void should_output_right_result_when_statistical_form_given_director_manage_1_manager_and_1_locker() {
-        String exceptedResult = "M 6 20\n" +
-                "  L 2 10\n" +
-                "  R 4 10\n" +
-                "    L 4 10\n";
+        String exceptedResult =
+                "" +
+                        "M 6 20\n" +
+                        "  L 2 10\n" +
+                        "  R 4 10\n" +
+                        "    L 4 10\n";
         LockerRobotDirector director = new LockerRobotDirector(
                 Collections.singletonList(new LockerRobotManager(Collections.singletonList(LockerUtil.getStoredLocker(10 - 2, 10)),
                         Collections.singletonList(new PrimaryLockerRobot(Collections.singletonList(LockerUtil.getStoredLocker(10 - 4, 10)))))));
@@ -35,4 +39,44 @@ public class LockerRobotDirectorTest {
         Assertions.assertEquals(exceptedResult, result);
     }
 
+    @Test
+    public void should_output_right_result_when_statistical_form_given_director_manage_two_robots() {
+        String exceptedResult =
+                "" +
+                        "M 12 40\n" +
+                        "  R 4 20\n" +
+                        "    L 2 10\n" +
+                        "    L 2 10\n" +
+                        "  R 8 20\n" +
+                        "    L 4 10\n" +
+                        "    L 4 10\n";
+        LockerRobotManager manager = new LockerRobotManager(Collections.emptyList(),
+                Arrays.asList(new PrimaryLockerRobot(Arrays.asList(
+                        LockerUtil.getStoredLocker(10 - 2, 10),
+                        LockerUtil.getStoredLocker(10 - 2, 10))),
+                        new PrimaryLockerRobot(Arrays.asList(
+                                LockerUtil.getStoredLocker(10 - 4, 10),
+                                LockerUtil.getStoredLocker(10 - 4, 10)))));
+        LockerRobotDirector director = new LockerRobotDirector(Collections.singletonList(manager));
+        String result = director.statisticalForm();
+        Assertions.assertEquals(exceptedResult, result);
+    }
+
+    @Test
+    public void should_output_right_result_when_statistical_form_given_director_manage_one_manager_that_manage_two_lockers_and_not_manager_one_locker_and_one_robot() {
+        String exceptedResult =
+                "" +
+                        "M 6 20\n" +
+                        "  L 2 10\n" +
+                        "  L 4 10\n";
+        LockerRobotManager manager = new LockerRobotManager(
+                Arrays.asList(LockerUtil.getStoredLocker(10 - 2, 10),
+                        LockerUtil.getStoredLocker(10 - 4, 10)),
+                Collections.emptyList());
+        Locker unManagedLocker = LockerUtil.getStoredLocker(10 - 3, 10);
+        LockerRobotBase unManagedRobot = new PrimaryLockerRobot(Collections.singletonList(LockerUtil.getStoredLocker(10 - 5, 10)));
+        LockerRobotDirector director = new LockerRobotDirector(Collections.singletonList(manager));
+        String result = director.statisticalForm();
+        Assertions.assertEquals(exceptedResult, result);
+    }
 }
